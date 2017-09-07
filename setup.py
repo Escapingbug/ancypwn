79c256116f40bac1b5a09ea58600880f534d173f
Anciety <ding641880047@126.com>
2017-09-07 21:34:24 +0800

    Updated dockerfile to make image smaller(which is still huge, about 900MB), and fixed setup.py 	modified:   Dockerfile 	new file:   ancypwn.py 	new file:   dist/ancypwn-0.1-py3.6.egg 	modified:   setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='ancypwn',
+    version='0.1',
+    descrption='Ancypwn docker image manipulation script, to simplify docker pwn environment management',
+    url='https://github.com/Escapingbug/ancypwn',
+    author='Anciety',
+    author_email='ding641880047@126.com',
+    py_modules=['ancypwn'],
+    entry_points = {
+        'console_scripts': ['ancypwn=ancypwn:main']
+    }
+)
